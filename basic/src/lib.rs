2f578faa0230cdda7d846143a66722043cdbf6dc
魏曹先生 <1992414357@qq.com>
2026-08-09 19:16:41 +0800

    feat: hide completion directory when completion is not enabled

--- basic/src/lib.rs
+++ basic/src/lib.rs
@@ -1,5 +1,14 @@
 use mingling::prelude::*;
 
+pub(crate) mod command;
+??? >>> COMPLETION
+pub(crate) mod completion;
+??? <<<
+pub(crate) mod error;
 pub(crate) mod fallback;
+pub(crate) mod help;
+pub(crate) mod hook;
+pub(crate) mod resource;
+pub(crate) mod setup;
 
 gen_program!();
