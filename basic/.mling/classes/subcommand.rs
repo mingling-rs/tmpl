01b16f3e2df2b0541fcd37d59aa3e0eeba6964f1
魏曹先生 <1992414357@qq.com>
2026-08-09 19:48:05 +0800

    feat(classes): add subcommand template
    Add a new class template for generating subcommands with a placeholder
    implementation.

--- basic/.mling/classes/subcommand.rs
+++ basic/.mling/classes/subcommand.rs
@@ -1,1 +1,8 @@
+use crate::*;
+use mingling::macros::command;
 
+#[command]
+pub fn <<<snake_case>>>(args: Entry) {
+    // TODO: Implement the behavior of the `<<<snake_case>>>` command
+    todo!()
+}
