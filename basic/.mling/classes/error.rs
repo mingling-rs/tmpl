f2c911049a1e9bd7054234e50751b647d5d9ee30
魏曹先生 <1992414357@qq.com>
2026-08-09 20:12:41 +0800

    feat: add error class template

---
+++ basic/.mling/classes/error.rs
@@ -0,0 +1,12 @@
+use mingling::{macros::renderer, Grouped};
+
+#[derive(Debug, Clone, Grouped, PartialEq, Eq)]
+pub struct Error<<<pascal_case>>> {
+    pub error_info: String,
+}
+
+#[renderer]
+pub fn handle_error_<<<snake_case>>>(error: Error<<<pascal_case>>>) {
+    // TODO: Implement handling logic for error `Error<<<pascal_case>>>`
+    todo!()
+}
